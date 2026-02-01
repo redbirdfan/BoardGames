e3553f300a13a29d98a0d8fa5e18b0e47772953c
redbirdfan <redbirdfan38@gmail.com>
2026-02-01 16:40:50 -0600

    scorecard started for me own visual purpose, dice being locked and unlocked on click

---
+++ boardgames/src/components/yahtzee/Scorecard.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const rollScore = 0;
+    const totalScore = 0;
+</script>
+
+<template>
+    <Div class="upperScorecard" style="display: inline-block; vertical-align: top; width: 45%;">
+        <p>Total Ones</p>
+        <p>Total Twos</p>
+        <p>Total Threes</p>
+        <p>Total Fours</p>
+        <p>Total Fives</p>
+        <p>Total Sixes</p>
+        <p>Upper Total</p>
+        <p>Bonus > (63)</p>
+        <p>Upper Total</p>
+    </Div>
+    <Div style="display: inline-block; vertical-align: top; width: 45%; padding-left: 40px">
+        <p>3 of a Kind</p>
+        <p>4 of a Kind</p>
+        <p>Small Stright</p>
+        <p>Large Straight</p>
+        <p>Full House</p>
+        <p>Chance</p>
+        <p>Yahtzee</p>
+        <p>Bonus Yahtzee</p>
+        <p>Lower Total</p>
+    </Div>
+</template>
--- boardgames/src/components/yahtzee/Yahtzee.vue
+++ boardgames/src/components/yahtzee/Yahtzee.vue
@@ -3,27 +3,48 @@
 
     import './Yahtzee.css'
     import DiceRoll from './DiceRoll.vue';
+    import Scorecard from './Scorecard.vue';
 
     const dice =ref([0, 0, 0, 0, 0])
-    
+    const lockedDice=([false, false, false, false, false])
+    let turnRoll = 1;
+
     const updateDice = (newDice)=>{
+        if(turnRoll < 3){
         dice.value = newDice
-        console.log(newDice)
+        return turnRoll = turnRoll+1
+        console.log(turnRoll)
+        } else { 
+            console.log("max turns reached")
+        }
+    }
+
+    const diceLock = (index)=> {
+        if(turnRoll >= 1 ){
+           if(lockedDice[index] === true){
+                lockedDice[index] = false
+                console.log(index, lockedDice[index])
+                
+            } else lockedDice[index] = true
+        }
     }
 </script>
 
 <template>
     <div class="dice-container">
-       <div v-for="(die, index) in dice" :key="index" class="dice-face">
-        {{ die === 0 ? '?' : die }}
+       <div 
+        v-for="(die, index) in dice" 
+        :key="index" 
+        class="dice-face"
+        :class="{ 'is-locked': lockedDice[index] }"
+        @click="diceLock(index)"
+        >
+        {{ die === 0 ? 'Yahtzee' : die }}
        </div>
     </div>
 
-    <DiceRoll @diceRolled="updateDice" />
+    <DiceRoll @diceRolled="updateDice" :rollCount="turnRoll"/>
 
-    <div class="score-container">
-            <p>Scorecard</p>
-    </div>
-
-    
+    <Scorecard />
+            
 </template>
