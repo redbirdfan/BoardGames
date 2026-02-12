6e56271cbe99f482a093d8fea16694ddf2a66edb
redbirdfan <redbirdfan38@gmail.com>
2026-02-11 21:07:37 -0600

    Scorecard visual looking better.  Border, split of top and bottom card.  Made adjustments to css.  Need to center in the game window.

--- boardgames/src/components/yahtzee/Scorecard.vue
+++ boardgames/src/components/yahtzee/Scorecard.vue
@@ -4,26 +4,56 @@
 </script>
 
 <template>
-    <Div class="upperScorecard" style="display: inline-block; vertical-align: top; width: 45%;">
-        <p>Total Ones</p>
-        <p>Total Twos</p>
-        <p>Total Threes</p>
-        <p>Total Fours</p>
-        <p>Total Fives</p>
-        <p>Total Sixes</p>
-        <p>Upper Total</p>
-        <p>Bonus > (63)</p>
-        <p>Upper Total</p>
-    </Div>
-    <Div style="display: inline-block; vertical-align: top; width: 45%; padding-left: 40px">
-        <p>3 of a Kind</p>
-        <p>4 of a Kind</p>
-        <p>Small Stright</p>
-        <p>Large Straight</p>
-        <p>Full House</p>
-        <p>Chance</p>
-        <p>Yahtzee</p>
-        <p>Bonus Yahtzee</p>
-        <p>Lower Total</p>
-    </Div>
+    <div class="score-container">
+        <p>Total Ones:</p>
+        <p>Total Twos:</p>
+        <p>Total Threes:</p>
+        <p>Total Fours:</p>
+        <p>Total Fives:</p>
+        <p>Total Sixes:</p>
+        <p>Upper Total:</p>
+        <p>Bonus > (63):</p>
+        <p>Upper Total:</p>
+    </div>
+
+    <div class="score-container">
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+    </div>
+
+    <div class="score-container" style="padding-left: 25px;">
+        <p>3 of a Kind:</p>
+        <p>4 of a Kind:</p>
+        <p>Small Stright:</p>
+        <p>Large Straight:</p>
+        <p>Full House:</p>
+        <p>Chance:</p>
+        <p>Yahtzee:</p>
+        <p>Bonus Yahtzee:</p>
+        <p>Lower Total:</p>
+    </div>
+    
+        <div class="score-container" style="padding-left: 25px;">
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+        <p>score</p>
+    </div>
+
+    <div>
+        <p>Total</p>
+    </div>
+
 </template>
